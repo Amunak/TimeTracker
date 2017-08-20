c7b0058615f5dd1c19d367c8712621b56b36728b
Amunak <git@amunak.net>
2017-08-20 18:55:57 +0000

    Adds readme (user documentation) with some screenshots

--- TimeTracker/Properties/AssemblyInfo.cs
+++ TimeTracker/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("1.4.*")]
 [assembly: NeutralResourcesLanguage("")]
 
